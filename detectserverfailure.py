4abb6c8044b57a972b468d19224c7393b7693b99
wizard <wizardcxy@126.com>
2014-12-18 20:40:51 +0800

    more usable

--- detectserverfailure.py
+++ detectserverfailure.py
@@ -15,11 +15,21 @@ password = "2784a48"
 mailserver = smtplib.SMTP(SMTPserver, 25)
 mailserver.login(sender, password)
 
+failcount=0;
+
 while True:
     response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
 
     if response == 0:
-        pass
+        if failcount > 0:
+            failcount = 0
+            message = 'deeply server is up'
+            msg = MIMEText(message)
+
+            msg['Subject'] = '199 Server is up'
+            msg['From'] = sender
+            msg['To'] = destination
+            mailserver.sendmail(sender, [sender], msg.as_string())
     else:
         message = 'deeply server is down'
         msg = MIMEText(message)
@@ -28,7 +38,10 @@ while True:
         msg['From'] = sender
         msg['To'] = destination
 
-        mailserver.sendmail(sender, [sender], msg.as_string())
+        if failcount < 10 :
+            failcount++
+            mailserver.sendmail(sender, [sender], msg.as_string())
+
 
 
     time.sleep(60)
