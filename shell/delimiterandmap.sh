fd8098cf5e9bb70fbb56bca0e7153b979e1a2661
wizard <wizardcxy@126.com>
2015-02-09 21:39:42 +0800

    change file name and add shell func

--- shell/delimiterandmap.sh
+++ shell/delimiterandmap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #data="111,222,333,444,555,666,10.10.103.223,10.10.103.162,10.10.103.224"
-data="111 222 333 10.10.103.223 10.10.103.162 10.10.103.224"
+#data="111 222 333 10.10.103.223 10.10.103.162 10.10.103.224"
+read -p "enter ips > " data
 # split data with ','
 oldIFS=$IFS  #定义一个变量为默认IFS
 #IFS=,        #设置IFS为逗号
@@ -30,4 +31,8 @@ echo $cluster
 for key in ${!mm[@]}; do
     echo "$key value ${mm[$key]} "
 done
+
+read -p "enter a ip" IP
+
+echo ${mm[$IP]}
 #IFS=$oldIFS  #还原IFS为默认值
---
+++ shell/func.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function fun1 {
+   echo "func 1"
+}
+function fun2  {
+   echo "func 2"
+}
+
+fun1
+fun2
