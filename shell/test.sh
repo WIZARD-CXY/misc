8ba43dbb638d159d6704f4b6ae8b893501e99ae3
wizard <wizardcxy@126.com>
2015-02-09 17:05:29 +0800

    add shell script to handle split and map function

---
+++ shell/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#data="111,222,333,444,555,666,10.10.103.223,10.10.103.162,10.10.103.224"
+data="111 222 333 10.10.103.223 10.10.103.162 10.10.103.224"
+# split data with ','
+oldIFS=$IFS  #定义一个变量为默认IFS
+#IFS=,        #设置IFS为逗号
+# default delimiter is ' '
+
+ii=1
+cluster=""
+declare -A mm
+for i in $data
+do
+    echo $i
+    name="infra"$ii
+    item="$name=$i"
+    echo $item
+    if [ "$ii" == 1 ]; then 
+        cluster=$item
+    else
+        cluster="$cluster,$item"
+    fi
+    mm[$i]=$name
+    let ii++
+
+ done
+echo $cluster
+
+for key in ${!mm[@]}; do
+    echo "$key value ${mm[$key]} "
+done
+#IFS=$oldIFS  #还原IFS为默认值
