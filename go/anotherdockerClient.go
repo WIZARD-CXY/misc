30708b5ef6e78df0bf90507bcbea5ba400f2ad0d
wizard <wizard_cxy@hotmail.com>
2015-03-26 14:09:56 +0800

    modify and add conf

--- go/anotherdockerClient.go
+++ go/anotherdockerClient.go
@@ -6,7 +6,7 @@ import "encoding/json"
 
 func main() {
 
-	jsonBody := &dockerclient.ContainerConfig{}
+	jsonBody := dockerclient.ContainerConfig{}
 
 	requestBody := `{"Binds":["/var/lib/kubelet/pods/ad61ca05-cf75-11e4-928c-f8bc12984e72/containers/master/ad39d1b5340050f16abdebe8e949cb3713bcf9775494b68455e62f65d2970e65:/dev/termination-log"],"NetworkMode":"container:17d6b9a3344a31fc0a63d0c610d0ee016ac720149ae4178c0c98151a63b91ade","IpcMode":"container:17d6b9a3344a31fc0a63d0c610d0ee016ac720149ae4178c0c98151a63b91ade","RestartPolicy":{}}`
 
--- go/test.go
+++ go/test.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 import (
+	"even"
 	"os"
 )
 
 func main() {
+	fmt.Println(even.Even(2))
 	fmt.Println(os.Getenv("DOCKER_HOST"))
 }
