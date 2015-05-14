6e063d41fe575b0b1473aa50341d2cb95bc28642
wizard <wizard_cxy@hotmail.com>
2015-05-14 09:39:15 +0800

    add signal handling

--- go/mkdir.go
+++ go/mkdir.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	os.Mkdir("astaxie", 0777)
 	os.MkdirAll("astaxie/test1/test2", 0777)
+
+	defer os.RemoveAll("astaxie")
 	err := os.Remove("astaxie")
 	if err != nil {
 		fmt.Println(err)
 	}
-	time.Sleep(time.Second * 10)
 
-	defer os.RemoveAll("astaxie")
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		log.Println("program killed")
+		os.RemoveAll("astaxie")
+
+		os.Exit(0)
+	}()
+
+	time.Sleep(time.Second * 10)
 }
