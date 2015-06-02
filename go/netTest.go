69e8db12a6d0242a3ce4bf2c1b3acd912245df4e
wizard <wizard_cxy@hotmail.com>
2015-06-02 20:26:13 +0800

    add nettest

--- go/netTest.go
+++ go/netTest.go
@@ -1,16 +1,40 @@
 package main
 
 import "net"
-import "math"
+import "fmt"
+import "encoding/binary"
+import "bytes"
 
 func main() {
-	cidr := "192.168.0.1/11"
-	_, addr, _ := net.ParseCIDR(cidr)
-	mask, _ := addr.Mask.Size()
-	println(mask)
-	if addr.IP.To4() != nil {
-		print(math.Pow(2, float64(32-mask)))
+	//string convert to net.IP ,then convert to uint32
+	cidr := "255.0.0.0/11"
+	_, ipNet, _ := net.ParseCIDR(cidr)
+
+	var num uint32
+
+	buf := bytes.NewBuffer(ipNet.IP)
+
+	err := binary.Read(buf, binary.BigEndian, &num)
+	if err == nil {
+		fmt.Println("haha", num)
 	} else {
-		print(math.Pow(2, float64(128-mask)))
+		fmt.Println(err)
 	}
+	num += 1
+
+	fmt.Println("haha", num)
+
+	buf2 := new(bytes.Buffer)
+	err = binary.Write(buf2, binary.BigEndian, num)
+
+	if err == nil {
+		fmt.Printf("%#v", buf2.Bytes())
+	} else {
+		fmt.Println(err)
+	}
+
+	// []byte convert to net.IP then convert to string
+	ip2 := net.IP(buf2.Bytes()).String()
+	fmt.Println(ip2)
+
 }
