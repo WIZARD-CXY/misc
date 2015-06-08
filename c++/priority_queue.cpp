d221d9c98b645d163978249f7d9ce5e4b995ee07
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-08 10:53:01 +0800

    add priority_queue aand functor

--- c++/priority_queue.cpp
+++ c++/priority_queue.cpp
@@ -1,9 +1,32 @@
-#include<priority_queue>
-
+#include<queue>
+#include<iostream>
+#include<vector>
+using namespace std;
 struct cmp{
 	bool operator() (const int a, const int b){
 		return a%10 > b%10; 
 	}
 };
 
-priority_queue<int, vector<int>, cmp> q; // gewei digit bigger priority is smaller
+int main(){
+    vector<int> vec;
+
+    vec.push_back(18);
+    vec.push_back(10);
+    vec.push_back(11);
+    vec.push_back(72);
+    vec.push_back(109);
+    vec.push_back(14);
+    
+    priority_queue<int, vector<int> ,cmp> pq;
+
+    for(int i=0; i<vec.size(); i++){
+        pq.push(vec[i]);
+    }
+
+    while(!pq.empty()){
+        int temp=pq.top();
+        pq.pop();
+        cout<<temp<<endl;
+    }
+}
