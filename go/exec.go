3b2dbd54596a03c383b912a7c92c9ed628ad5a2f
wizard <wizard_cxy@hotmail.com>
2015-07-11 20:34:45 +0800

    add net monitor

--- go/exec.go
+++ go/exec.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	path, err := exec.LookPath("iptables")
+	path, err := exec.LookPath("ifconfig")
 	if err != nil {
 		log.Fatal("installing fortune is in your future")
 	}
-	fmt.Printf("fortune is available at %s\n", path)
+	output, _ := exec.Command(path, "eth1").CombinedOutput()
+
+	//ifconfig "$interface" | grep "TX bytes" | cut -d: -f3 | awk '{ print $1 }'
+	ss := string(output[:len(output)-1])
+	fmt.Println(ss)
+	idx := strings.Index(ss, "RX bytes:")
+	lastline := ss[idx:]
+	aa := strings.Split(lastline, " ")
+	rxbytes := strings.Split(aa[1], ":")[1]
+	txbytes := strings.Split(aa[6], ":")[1]
+	fmt.Println(strings.Split(aa[1], ":")[1], strings.Split(aa[6], ":")[1])
+	rx, _ := strconv.ParseUint(rxbytes, 10, 64) //trim trailing '/n'
+	tx, _ := strconv.ParseUint(txbytes, 10, 64) //trim trailing '/n'
+	fmt.Println(rx, tx)
+
 }
