c66d8ba780b5d32d0c0605734f1d4cb075a6220e
chenxingyu <wizard_cxy@hotmail.com>
2016-05-11 13:35:02 +0800

    add new knowledge

---
+++ go/structwithembededinterface.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+// some interface
+type Stringer interface {
+	String() string
+}
+
+// a struct that implements Stringer interface
+type Struct1 struct {
+	field1 string
+}
+
+func (s Struct1) String() string {
+	return s.field1
+}
+
+// another struct that implements Stringer interface, but has a different set of fields
+type Struct2 struct {
+	field1 []string
+	dummy  bool
+}
+
+func (s Struct2) String() string {
+	return fmt.Sprintf("%v, %v", s.field1, s.dummy)
+}
+
+// container that can embedd any struct which implements Stringer interface
+type StringerContainer struct {
+	Stringer
+	haha
+}
+
+// you can change the default sayxixi implementation
+func (StringerContainer) sayxixi() {
+	fmt.Println("xixihaha")
+}
+
+type haha interface {
+	sayhaha()
+	sayxixi()
+}
+type S struct {
+}
+
+func (S) sayhaha() {
+	fmt.Println("sayhaha")
+}
+func (S) sayxixi() {
+	fmt.Println("sayxixi")
+}
+
+func main() {
+	// the following prints: This is Struct1
+	aa := StringerContainer{Struct1{"This is Struct1"}, S{}}
+	aa.sayhaha()
+	aa.sayxixi()
+	fmt.Println(aa)
+
+	// the following prints: [This is Struct1], true
+	//fmt.Println(StringerContainer{Struct2{[]string{"This", "is", "Struct1"}, true}})
+	// the following does not compile:
+	// cannot use "This is a type that does not implement Stringer" (type string)
+	// as type Stringer in field value:
+	// string does not implement Stringer (missing String method)
+	//fmt.Println(StringerContainer{"This is a type that does not implement Stringer"})
+}
