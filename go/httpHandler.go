98a366baa84f01586c6d5281506b447b792ca6ed
chenxingyu <wizard_cxy@hotmail.com>
2017-05-22 17:32:15 +0800

    learn httphandler more

---
+++ go/httpHandler.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func timeHandler(format string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tm := time.Now().Format(format)
+		w.Write([]byte("The time is: " + tm))
+	})
+}
+
+func main() {
+	var format string = time.RFC1123
+	th := timeHandler(format)
+
+	http.Handle("/time", th)
+	log.Println("Listening...")
+
+	http.ListenAndServe(":3000", nil)
+}
