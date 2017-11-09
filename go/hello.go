d60815168761a7db8c8652991532f074ffed5cd6
chenxingyu <wizard_cxy@hotmail.com>
2017-11-09 15:36:02 +0800

    add interface test

--- go/hello.go
+++ go/hello.go
@@ -10,12 +10,24 @@ import (
 func hello() {
 	fmt.Println("Hello world")
 }
-
+type A struct {
+	DD int
+	CC string
+}
 func main() {
 	hl := hello
 	fv := reflect.ValueOf(hl)
 
 	fmt.Println("fv is reflect.Func ?", fv.Kind() == reflect.Func)
+	i := A{}
+	iv := reflect.ValueOf(i)
+	t:= iv.Type()
+	fmt.Println(t)
+
+	for i:=0; i < iv.NumField(); i++{
+		f := iv.Field(i)
+		fmt.Printf("%d: %s %s = %#v\n",i, t.Field(i).Name, f.Type(), f.Interface())
+	}
 	fv.Call(nil)
 
 	slc := []int{1, 2, 3, 4, 5}
